69d5f07a0dfe472fdf375fee2908d48109b7e69e
黄云辉 <huangyunhui@national-space.com>
2021-05-06 15:27:02 +0800

    🐳 chore: 优化rollup构建
    1. 编译目标改为ES5，向下兼容老版浏览器
    2. rollup打包区分环境
    3. 本地环境下启用服务器、关闭压缩
    4. 生产环境关闭服务器、启用压缩

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,15 +1,21 @@
+// 本地测试环境-只打包umd模块，并开启本地服务器
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
-import {uglify} from 'rollup-plugin-uglify';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
 import packageConfig from './package.json';
 
 const extensions = ['.js', '.ts', '.tsx'];
 
-// 公共配置
-const commonConfig = {
+export default {
   input: 'src/index.ts',
+  output: {
+    name: packageConfig.name,
+    file: packageConfig.main, // 通用模块
+    format: 'umd',
+  },
   plugins: [
     resolve(extensions), // 查找和打包node_modules中的第三方模块
     commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
@@ -21,27 +27,12 @@ const commonConfig = {
       extensions,
       useTsconfigDeclarationDir: true,
     }),
-    uglify(), // 压缩
+    livereload(), // 热更新，与serve结合使用
+    serve({
+      open: true, // 自动打开页面
+      port: 8000,
+      openPage: '/index.html', // 打开的页面
+      contentBase: '',
+    }),
   ],
 };
-
-// 需要导出的模块类型
-const outputModel = [
-  {
-    file: packageConfig.main, // 通用模块
-    format: 'umd',
-  },
-  {
-    file: packageConfig.module, // es6模块
-    format: 'es',
-  },
-];
-
-export default outputModel.map(output => {
-  return Object.assign({}, commonConfig, {
-    output: {
-      name: packageConfig.name,
-      ...output,
-    },
-  });
-});
