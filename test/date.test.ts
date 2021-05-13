1a416841e921917226b335edb5d1125b99a237c3
黄云辉 <huangyunhui@national-space.com>
2021-05-13 18:27:15 +0800

    🧪 test: 格式化日期测试用例

--- test/date.test.ts
+++ test/date.test.ts
@@ -2,16 +2,20 @@ import {formatDate} from '../src/index';
 
 describe('#formatDate', () => {
   const date = '2020-04-01 12:01:01';
-  test(`formatDate(${new Date(date)}) is ${date}`, () => {
+  test(`formatDate(${new Date(date)})`, () => {
     expect(formatDate(new Date(date))).toBe('2020-04-01 12:01:01');
   });
-  test(`formatDate(${new Date(date)}, YYYY-MM-dd) is 2020-04-01`, () => {
+  test(`formatDate(${new Date(date)}, YYYY-MM-dd)`, () => {
     expect(formatDate(new Date(date), 'YYYY-MM-dd')).toBe('2020-04-01');
   });
-  test(`formatDate(${new Date(date)}, HH:mm:ss) is 12:01:01`, () => {
+  test(`formatDate(${new Date(date)}, HH:mm:ss)`, () => {
     expect(formatDate(new Date(date), 'HH:mm:ss')).toBe('12:01:01');
   });
-  test(`formatDate(${new Date(date)}, YYYY-M-d H:m:s) is 2020-4-1 12:1:1`, () => {
+  test(`formatDate(${new Date(date)}, YYYY-M-d H:m:s)`, () => {
     expect(formatDate(new Date(date), 'YYYY-M-d H:m:s')).toBe('2020-4-1 12:1:1');
+  });
+  test(`formatDate()`, () => {
+    const currentDate = formatDate();
+    expect(currentDate).toBe(formatDate(new Date(currentDate)));
   });
 });
