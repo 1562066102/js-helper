a1109bedd5ef3fd6c8d423767130181f1470e545
黄云辉 <huangyunhui@national-space.com>
2021-05-18 16:20:25 +0800

    🐞 fix: 修复节流函数边界调用多执行一次

--- src/throttle.ts
+++ src/throttle.ts
@@ -16,13 +16,13 @@ export default function throttle(fn: ThrottleCallback, delay = 200): Throttle {
   return function (...args) {
     const self = this;
     const now = new Date().getTime();
+    clearTimeout(timer);
     if (!last || now - last >= delay) {
       // !last - 确保开始边界上的调用
       last = now;
       fn.apply(self, args);
     } else if (now - last < delay) {
       // 低于节流阀时间内 - 确保结尾边界上的调用
-      clearTimeout(timer);
       timer = setTimeout(function () {
         timer = last = null;
         fn.apply(self, args);
