cf7fb46da82797b2e945e9f4a398ffa6f6eb90bd
黄云辉 <huangyunhui@national-space.com>
2021-05-19 17:50:56 +0800

    ✨ feat: 新增图片加载、base64-file互转

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,5 @@ export {default as debounce} from './debounce';
 export {default as throttle} from './throttle';
 export {add, subtract, multiply, divide} from './compute';
 export {firstLetterUpperCase} from './string';
+export {default as loadImage} from './load-image';
+export {base64ToFile, fileToBase64} from './transform-base64-file';
