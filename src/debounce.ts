abdb685620597be41d18a1fb48a804b017e336aa
黄云辉 <huangyunhui@national-space.com>
2021-05-21 17:54:31 +0800

    ✨ feat: debounce方法采用泛型声明

--- src/debounce.ts
+++ src/debounce.ts
@@ -2,7 +2,7 @@
 export type Debounce = (...args: any[]) => void;
 
 /** 防抖函数接收的回调函数类型 */
-export type DebounceCallback = (...args: any[]) => any;
+export type DebounceCallback = (...args: any[]) => void;
 
 /**
  * 防抖函数
@@ -11,8 +11,8 @@ export type DebounceCallback = (...args: any[]) => any;
  * @param delay 最小间隔时间，默认为200ms
  * @returns 函数
  */
-export default function debounce(fn: DebounceCallback, delay = 200): Debounce {
-  let timer: any = null;
+export default function debounce<T extends DebounceCallback>(fn: T, delay = 200): Debounce {
+  let timer: NodeJS.Timeout | null = null;
   return function (...args) {
     const self = this;
     if (timer) {
