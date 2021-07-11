955283ab09496660031abcb93e936e519981875f
huangyunhui <1562066102@qq.com>
2021-07-11 22:35:46 +0800

    🎈 perf(): 优化clone函数耦合度

--- src/clone.ts
+++ src/clone.ts
@@ -4,17 +4,17 @@ import {getType, isReferenceType, Type} from './get-type';
  * 深拷贝方法
  * @description 拷贝一个对象，得到一个值一样的全新对象
  * @param data 任何类型的值
- * @returns 值一样但引用地址不同的另一个对象
+ * @returns 值一样但引用地址不同的对象
  */
-export function clone<T = any>(data: T): T {
+export const clone = function cloneSourceFn<T>(data: T): T {
   if (isReferenceType(data)) {
     const type = getType(data);
     const newObj: any = type === Type.Object ? {} : [];
     for (const key in data) {
-      newObj[key] = clone(data[key]);
+      newObj[key] = cloneSourceFn(data[key]);
     }
     return newObj;
   } else {
     return data;
   }
-}
+};
