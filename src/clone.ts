4af88d36b89e5473f3a52e94823a84c5df642190
1562066102 <1562066102@qq.com>
2021-07-23 15:23:50 +0800

    🐞 fix(clone): 修复克隆部分类型对象错误
    克隆错误的对象类型：
    1. Date
    2. RegExp
    3. Function

--- src/clone.ts
+++ src/clone.ts
@@ -1,13 +1,14 @@
-import {isObject, isReference} from './type';
+import {isObject, isArray} from './type';
 
 /**
  * 深拷贝
- * @description 拷贝一个对象，得到一个值一样的全新对象
+ * @description 拷贝一个对象，得到一个值一样的全新对象（注：此方法并没有拷贝对象原型链上的属性）
  * @param data 任何类型的值
  * @returns 值一样但引用地址不同的对象
  */
 export const clone = function cloneSourceFn<T>(data: T): T {
-  if (!isReference(data)) return data;
+  const isObjOrArr = isObject(data) || isArray(data);
+  if (!isObjOrArr) return data;
   const newObj: any = isObject(data) ? {} : [];
   for (const key in data) {
     newObj[key] = cloneSourceFn(data[key]);
