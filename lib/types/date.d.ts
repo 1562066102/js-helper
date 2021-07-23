e8d17cadf456680a5b6794b01927e11878712f28
1562066102 <1562066102@qq.com>
2021-07-23 15:28:14 +0800

    🔧 build(): 打包1.1.0

--- lib/types/date.d.ts
+++ lib/types/date.d.ts
@@ -1,7 +1,7 @@
 /**
- * 格式化日期函数
+ * 格式化日期
  * @param date 日期，默认为当前时间
- * @param format 格式，默认为'YYYY-MM-dd HH:mm:ss'
+ * @param format 日期格式，默认为'YYYY-MM-dd HH:mm:ss'，不补零的格式为'YYYY-M-d H:m:s'
  * @return 格式化后的日期字符串
  */
 export declare function formatDate(date?: Date, format?: string): string;
--- lib/types/math.d.ts
+++ lib/types/math.d.ts
@@ -1,4 +1,32 @@
 /**
+ * 加法
+ * @param args1 数字1
+ * @param args2 数字2
+ * @returns 计算后的值
+ */
+export declare const add: (args_0: number, args_1: number) => number;
+/**
+ * 减法
+ * @param args1 数字1
+ * @param args2 数字2
+ * @returns 计算后的值
+ */
+export declare const subtract: (args_0: number, args_1: number) => number;
+/**
+ * 乘法
+ * @param args1 数字1
+ * @param args2 数字2
+ * @returns 计算后的值
+ */
+export declare const multiply: (args_0: number, args_1: number) => number;
+/**
+ * 除法
+ * @param args1 数字1
+ * @param args2 数字2
+ * @returns 计算后的值
+ */
+export declare const divide: (args_0: number, args_1: number) => number;
+/**
  * 生成指定范围内的随机整数
  * @param min 范围内最小整数
  * @param max 范围内最大整数
