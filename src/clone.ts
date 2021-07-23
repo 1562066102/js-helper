e0efed03fe69038f83f11fa254346db7a8d8ad35
1562066102 <1562066102@qq.com>
2021-07-23 14:02:08 +0800

    🦄 refactor(clone): 重构clone方法

--- src/clone.ts
+++ src/clone.ts
@@ -1,20 +1,16 @@
-import {getType, isReferenceType, Type} from './get-type';
+import {isObject, isReference} from './type';
 
 /**
- * 深拷贝方法
+ * 深拷贝
  * @description 拷贝一个对象，得到一个值一样的全新对象
  * @param data 任何类型的值
  * @returns 值一样但引用地址不同的对象
  */
 export const clone = function cloneSourceFn<T>(data: T): T {
-  if (isReferenceType(data)) {
-    const type = getType(data);
-    const newObj: any = type === Type.Object ? {} : [];
-    for (const key in data) {
-      newObj[key] = cloneSourceFn(data[key]);
-    }
-    return newObj;
-  } else {
-    return data;
+  if (!isReference(data)) return data;
+  const newObj: any = isObject(data) ? {} : [];
+  for (const key in data) {
+    newObj[key] = cloneSourceFn(data[key]);
   }
+  return newObj;
 };
